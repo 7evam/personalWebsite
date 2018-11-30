42078e7a7638e7ecd9f97fdecf6423faa4914839
Evan Lane <evanklane@yahoo.com>
2018-11-30 11:35:32 -0500

    added mobile responsiveness

--- script.js
+++ script.js
@@ -21,10 +21,24 @@ const code = document.querySelector('#codeHeader')
 const musicIcon = document.querySelector('#musicIcon')
 const codeIcon = document.querySelector('#codeIcon')
 const headline = document.querySelector('#headline')
+const pageInfo = document.querySelector('.pageInfo')
+const pageContent = document.querySelector('.page-content')
+const musicItems = document.querySelector('.musicItems')
 const musicMessage = "I’m a trumpet player, arranger, curator, and composer living in NYC since 2012. I regularly perform all over NYC and occasionally the world, and have recorded from everywhere from my home studio to Sony BMG studios. I pride myself on being able to fit in any musical setting and love playing in all genres."
 const welcomeMessage = "Welcome to my site. I am Evan. Isn't that great?"
 const devMessage = "I am a forward-thinking full-stack web developer passionate about solving problems collaboratively and fostering an empathetic work environment. My musical background encourages me to think creatively and work well as part of a team. I am especially interested in working with React and other Javascript-based technologies, but my favorite part of development is learning new technologies."
 
+function isOverflown(element) {
+    return element.scrollHeight > element.clientHeight;
+}
+
+function resizeInfo(){
+  if (isOverflown(pageInfo)){
+    pageInfo.style.height="auto"
+  } else {
+    pageInfo.style.height=""
+  }
+}
 
 function updatePage(){
   if(mySwiper.realIndex === 0){
@@ -34,6 +48,8 @@ function updatePage(){
     music.style.textDecoration = 'underline'
     code.style.textDecoration = 'none'
     headline.textContent = musicMessage
+    pageContent.style.overflow = 'scroll'
+    // musicItems.style.display = 'flex'
   } else if(mySwiper.realIndex === 1){
     music.style.color = 'black';
     home.style.color = 'red';
@@ -41,6 +57,8 @@ function updatePage(){
     music.style.textDecoration = 'none'
     code.style.textDecoration = 'none'
     headline.textContent = welcomeMessage
+    pageContent.style.overflow = 'hidden'
+    // musicItems.style.display = 'flex'
   } else if(mySwiper.realIndex === 2){
     music.style.color = 'black';
     home.style.color = 'black';
@@ -48,17 +66,22 @@ function updatePage(){
     music.style.textDecoration = 'none'
     code.style.textDecoration = 'underline'
     headline.textContent = devMessage
+    pageContent.style.overflow = 'scroll'
+    // musicItems.style.height = '10px'
   }
 }
 
 home.style.color = 'red'
-mySwiper.on("slideChange",updatePage);
+mySwiper.on("slideChange",updatePage, resizeInfo);
 
 music.addEventListener("click",function(){mySwiper.slideTo(0)})
 musicIcon.addEventListener("click",function(){mySwiper.slideTo(0)})
 home.addEventListener("click",function(){mySwiper.slideTo(1)})
 code.addEventListener("click",function(){mySwiper.slideTo(2)})
 codeIcon.addEventListener("click",function(){mySwiper.slideTo(2)})
+
+// resizeInfo()
+// window.addEventListener('resize', resizeInfo)
 
 
 
