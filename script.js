fd4d38a90fb1cbcf333072d326a438c820009ef0
Evan Lane <evanklane@yahoo.com>
2018-12-05 16:34:29 -0500

    added speech bubble, got rid of images

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ const headline = document.querySelector('#headline')
 const pageInfo = document.querySelector('.pageInfo')
 const pageContent = document.querySelector('.page-content')
 const musicItems = document.querySelector('.musicItems')
-const musicMessage = "I’m a trumpet player, arranger, curator, and composer living in NYC since 2012. I regularly perform all over NYC and occasionally the world, and have recorded from everywhere from my home studio to Sony BMG studios. I pride myself on being able to fit in any musical setting and love playing in all genres."
-const welcomeMessage = "Welcome to my site. I am Evan. Isn't that great?"
-const devMessage = "I am a forward-thinking full-stack web developer passionate about solving problems collaboratively and fostering an empathetic work environment. My musical background encourages me to think creatively and work well as part of a team. I am especially interested in working with React and other Javascript-based technologies, but my favorite part of development is learning new technologies."
+const musicMessage = "<p>I’m a trumpet player, arranger, curator, and composer living in NYC since 2012. I regularly perform all over NYC and occasionally the world, and have recorded from everywhere from my home studio to Sony BMG studios. I pride myself on being able to fit in any musical setting and love playing in all genres.</p>"
+const welcomeMessage = "<p class='highlight'>Hello!</p> <p>I'm Evan and I'm a web-developer, musician, fun guy, and fungi enthusiast.</p> <p>I am a forward-thinking full-stack web developer passionate about solving problems collaboratively and fostering an empathetic work environment. My musical background encourages me to think creatively and work well as part of a team. I am especially interested in working with React and other Javascript-based technologies, but my favorite part of development is learning new technologies.</p> <p>You can check out what I've built if you slide over to the right. I've also included some of the musical projects I've been a a part of on the left, if you're curious.</p>"
+const devMessage = "<p>I am a forward-thinking full-stack web developer passionate about solving problems collaboratively and fostering an empathetic work environment. My musical background encourages me to think creatively and work well as part of a team. I am especially interested in working with React and other Javascript-based technologies, but my favorite part of development is learning new technologies.</p>"
 
 function updatePage(){
   document.querySelector(".page-content").scrollTo(0,0)
@@ -32,7 +32,7 @@ function updatePage(){
     code.style.color = 'white';
     music.style.textDecoration = 'underline'
     code.style.textDecoration = 'none'
-    headline.textContent = musicMessage
+    headline.innerHTML = musicMessage
     pageContent.style.overflow = 'scroll'
     // musicItems.style.height = ''
     // musicItems.style.display = 'flex'
@@ -42,7 +42,7 @@ function updatePage(){
     code.style.color = 'white';
     music.style.textDecoration = 'none'
     code.style.textDecoration = 'none'
-    headline.textContent = welcomeMessage
+    headline.innerHTML = welcomeMessage
     pageContent.style.overflow = 'hidden'
     // musicItems.style.height = ''
     // musicItems.style.display = 'flex'
@@ -52,7 +52,7 @@ function updatePage(){
     code.style.color = '#DBCBD8';
     music.style.textDecoration = 'none'
     code.style.textDecoration = 'underline'
-    headline.textContent = devMessage
+    headline.innerHTML = devMessage
     pageContent.style.overflow = 'scroll'
     // musicItems.style.height = '1px'
   }
