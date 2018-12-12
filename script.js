d0ea9576adc55a022775741bd823b96a3eb9fd51
Evan Lane <evanklane@yahoo.com>
2018-12-12 11:12:28 -0500

    added arrows

--- script.js
+++ script.js
@@ -23,6 +23,9 @@ const musicItems = document.querySelector('.musicItems')
 const musicMessage = "<p>I’m a trumpet player, arranger, curator, and composer living in NYC since 2012. I regularly perform all over NYC and occasionally the world, and have recorded from everywhere from my home studio to Sony BMG studios. I pride myself on being able to fit in any musical setting and love playing in all genres.</p>"
 const welcomeMessage = "<p class='highlight'>Hello!</p> <p>I'm Evan and I'm a web-developer, musician, fun guy, and fungi enthusiast.</p> <p>I am a forward-thinking full-stack web developer passionate about solving problems collaboratively and fostering an empathetic work environment. My musical background encourages me to think creatively and work well as part of a team. I am especially interested in working with React and other Javascript-based technologies, but my favorite part of development is learning new technologies.</p> <p>You can check out what I've built if you slide over to the right. I've also included some of the musical projects I've been a a part of on the left, if you're curious.</p>"
 const devMessage = "<p>I am a forward-thinking full-stack web developer passionate about solving problems collaboratively and fostering an empathetic work environment. My musical background encourages me to think creatively and work well as part of a team. I am especially interested in working with React and other Javascript-based technologies, but my favorite part of development is learning new technologies.</p>"
+const right = document.querySelector('.right');
+const left = document.querySelector('.left');
+
 
 function updatePage(){
   document.querySelector(".page-content").scrollTo(0,0)
@@ -32,7 +35,9 @@ function updatePage(){
     code.style.color = 'white';
     music.style.textDecoration = 'underline'
     code.style.textDecoration = 'none'
-    headline.innerHTML = musicMessage
+    headline.innerHTML = musicMessage;
+    left.style.display = 'none';
+    right.style.display = ''
     // pageContent.style.overflow = 'scroll'
     // musicItems.style.height = ''
     // musicItems.style.display = 'flex'
@@ -40,9 +45,11 @@ function updatePage(){
     music.style.color = 'white';
     home.style.color = '#FF6B6B';
     code.style.color = 'white';
-    music.style.textDecoration = 'none'
-    code.style.textDecoration = 'none'
-    headline.innerHTML = welcomeMessage
+    music.style.textDecoration = 'none';
+    code.style.textDecoration = 'none';
+    headline.innerHTML = welcomeMessage;
+    right.style.display='';
+    left.style.display='';
     // pageContent.style.overflow = 'hidden'
     // musicItems.style.height = ''
     // musicItems.style.display = 'flex'
@@ -52,7 +59,9 @@ function updatePage(){
     code.style.color = '#FF6B6B';
     music.style.textDecoration = 'none'
     code.style.textDecoration = 'underline'
-    headline.innerHTML = devMessage
+    headline.innerHTML = devMessage;
+    right.style.display = 'none';
+    left.style.display = ''
     // pageContent.style.overflow = 'scroll'
     // musicItems.style.height = '1px'
   }
@@ -63,6 +72,8 @@ mySwiper.on("slideChange",updatePage);
 
 music.addEventListener("click",function(){mySwiper.slideTo(0)})
 musicIcon.addEventListener("click",function(){mySwiper.slideTo(0)})
+right.addEventListener("click",function(){mySwiper.slideNext()})
+left.addEventListener("click",function(){mySwiper.slidePrev()})
 home.addEventListener("click",function(){mySwiper.slideTo(1)})
 code.addEventListener("click",function(){mySwiper.slideTo(2)})
 codeIcon.addEventListener("click",function(){mySwiper.slideTo(2)})
